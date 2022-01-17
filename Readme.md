85c41a14deccf93fcb4b16ac5fe00af4d4cfa94f
Frankie Toledo <alexis.frank.toledo@gmail.com>
2022-01-17 04:03:00 -0300

    Fixes on relatives paths

--- Readme.md
+++ Readme.md
@@ -1,10 +1,40 @@
-#ABOUT
+# ABOUT
+Inspired by ninite on windows and for a personal necesities i made this tool for install unantended software selection.This is a weekend project and can be re-used for free or can send yours PR. *Pourly tested on Kubuntu 20.04LTS 😅*
 
-Inspired by ninite on windows and for a personal necesities i made this tool for install unantended software selection.
+## SOFTWARE INCLUDED
 
-this is a weekend project and can be re-used for free or can send your PR.
+| Software | Description |
+|--|--|
+| Anydesk | remote access tool|
+|Barrier|share mouse and keyboard across computers|
+|Blender|3d modeling suite|
+|Chrome|web browser|
+|Discord|gaming community software. Calls and more|
+|Flameshot|capture tool|
+|Gimp|image editor open source|
+|Git|software version control|
+|Inkscape|open source vector graphics editor |
+|Openvpn|a VPN |
+|Qbittorrent|P2P Multiplattform client|
+|Slack|messaging app for busisness|
+|Steam|gamming platform|
+|Telegram|messaging app|
+|VsCode|visual studio code - code editor|
+|Zoom|video conferences app|
 
-#USAGE
+## USAGE
 
-#SNAPSHOTS
+ 1. Clone the repo or download the .sh file
+ 2. Open a terminal and run the script as root
+>     sudo ./batchInstall.sh
+ 3. Choose an option: Install all software or select what will be installed
 
+## SNAPSHOTS
+
+![enter image description here](assets/screenshot01.png)
+![enter image description here](assets/screenshot02.png)
+![enter image description here](assets/screenshot03.png)
+
+## CONTACT ME
+|[![Twitter Logo](assets/twitter.png)](https://twitter.com/frankie_toledo)|[![LinkedIn Logo](assets/linkedin.png)](https://www.linkedin.com/in/frankie-toledo/)|
+|--|--|
--- batchInstal.sh
+++ batchInstal.sh
@@ -10,122 +10,148 @@ if [ "$EUID" -ne 0 ]
 fi
 
 ##Array of programs to be installed with description
-declare -a arrayOptions=("anydesk" "remote access tool" OFF 
-    "blender" "3d modeling suite" OFF 
-    "code" "visual studio code - code editor" OFF 
-    "discord" "gaming community software. Calls and more" OFF 
-    "flameshot" "capture tool" OFF 
-    "gimp" "image editor open source" OFF 
-    "git-all" "software version control" OFF 
-    "google-chrome-stable" "web browser" OFF 
-    "inkscape" "open source vector graphics editor " OFF 
-    "openvpn" "a VPN " OFF 
-    "qbittorrent" "P2P Multiplattform client" OFF 
-    "slack" "messaging app for busisness" OFF 
-    "steam" "gamming platform" OFF 
-    "synergy" "share mouse and keyboard across computers" OFF 
-    "telegram-desktop" "messaging app" OFF 
-    "zoom" "video conferences app" OFF 
+declare -a arrayOptions=("Anydesk" ".remote access tool" OFF 
+    "Barrier" ".share mouse and keyboard across computers" OFF 
+    "Blender" ".3d modeling suite" OFF 
+    "Chrome" ".web browser" OFF 
+    "Discord" ".gaming community software. Calls and more" OFF 
+    "Flameshot" ".capture tool" OFF 
+    "Gimp" ".image editor open source" OFF 
+    "Git" ".software version control" OFF 
+    "Inkscape" ".open source vector graphics editor " OFF 
+    "Openvpn" ".a VPN " OFF 
+    "Qbittorrent" ".P2P Multiplattform client" OFF 
+    "Slack" ".messaging app for busisness" OFF 
+    "Steam" ".gamming platform" OFF 
+    "Telegram" ".messaging app" OFF 
+    "VsCode" ".visual studio code - code editor" OFF 
+    "Zoom" ".video conferences app" OFF 
 )
 
+
 ##Function to check if pkg are installed
+#currently are not used because cannot trully detect when a package are installed
 function installed() {
   status="$(dpkg-query -W --showformat='${db:Status-Status}' "$1" 2>&1)"
   if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
-      false
-  else
       true
+  else
+      false
   fi
+}
+
+#print software options formates like whiptail textbox needed
+function printOptions(){
+    for i in "${arrayOptions[@]}"
+    do
+        if [[ $i  == "OFF" ]];then
+            printf "\n"
+        else
+            printf "%s " $i
+        fi
+    done
+}
+
+#store on array var only the keys of software list
+function getOnlyKeys(){
+    bAction=true
+    for i in "${arrayOptions[@]}"
+    do
+        if [[ $bAction == true ]]; then
+            pkgs+=("$i")
+            bAction=false
+        elif [[ $i  == "OFF" ]];then
+            bAction=true
+        fi
+    done
 }
 
 #Run the proper instalation of the software
 function runInstalation(){
   for pkg in ${pkgs[@]}; do
-      if $(installed $pkg) ; then
-          printf "[Info] Installing %s \n" $pkg
-          case $pkg in
-            *"anydesk"*)
-              wget -nc -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
-              echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk.list
-              apt update
-              apt install -y anydesk
-            ;;
-            *"blender"*)
-              apt-get install -y blender
-            ;;
-            (*"code"*)
-              apt-get install -y software-properties-common apt-transport-https
-              wget -qO- -nc https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-              install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-              sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-              apt update 
-              apt-get install -y code          
-            ;;
-            (*"discord"*)
-              wget -ncO discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
-              apt install -y discord.deb
-              rm discord.deb
-            ;;
-            (*"flameshot"*)
-              apt-get install -y flameshot
-            ;;
-            (*"gimp"*)
-              apt-get install -y gimp
-            ;;
-            (*"git-all"*)
-              apt-get install -y git-all
-            ;;
-            (*"google-chrome-stable"*)
-              wget -nc https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chromepkg
-              apt-get install -y chromepkg
-              rm chromepkg
-            ;;
-            (*"inkscape"*)
-              apt-get install -y inskcape
-            ;;
-            (*"openvpn"*)
-              apt-get -y install openvpn
-            ;;
-            (*"slack"*)
-              wget -nc https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
-              apt install -y slack-desktop-*.deb
-              rm slack-desktop-*
-            ;;
-            (*"steam"*)
-              wget -nc https://steamcdn-a.akamaihd.net/client/installer/steam.deb
-              apt install -y steam.deb
-            ;;
-            (*"synergy"*)
-              wget -nc https://github.com/brahma-dev/synergy-stable-builds/releases/download/v1.8.8-stable/synergy-v1.8.8-stable-Linux-x86_64.deb -o synergygraty
-              apt install -y synergygraty 
-              rm synergygraty
-            ;;
-            (*"telegram-desktop"*)
-              apt-get install -y telegram-desktop
-            ;;
-            (*"qbittorrent"*)
-              apt-get install -y qbittorrent
-            ;;
-            (*"zoom"*)
-              wget -nc https://zoom.us/client/latest/zoom_amd64.deb
-              apt install -y zoom_amd64.deb
-              rm zoom_amd64.deb
-            ;;
-            (*)
-              printf "[Warning] Option not reconigzed %s \n" $pkg
-            ;;
-          esac
-      else
-        printf "[Info] Skipping %s. Already installed \n" $pkg
-      fi
+        printf "[Info] Installing %s \n" $pkg
+        case $pkg in
+          *"Anydesk"*)
+            wget -nc -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+            echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk.list
+            apt update
+            apt install -y anydesk
+          ;;
+          (*"Barrier"*)
+            apt-get install -y barrier
+          ;;
+          (*"Blender"*)
+            apt-get install -y blender
+          ;;
+          (*"Chrome"*)
+            wget -nc https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chromepkg.deb
+            apt-get install -y ./chromepkg.deb
+            rm chromepkg.deb
+          ;;
+          (*"Discord"*)
+            wget -nc -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+            apt install -y ./discord.deb
+            rm discord.deb
+          ;;
+          (*"Flameshot"*)
+            apt-get install -y flameshot
+          ;;
+          (*"Gimp"*)
+            apt-get install -y gimp
+          ;;
+          (*"Git"*)
+            apt-get install -y git-all
+          ;;
+          (*"Inkscape"*)
+            apt-get install -y inkscape
+          ;;
+          (*"Openvpn"*)
+            apt-get -y install openvpn
+          ;;
+          (*"Slack"*)
+            wget -nc https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
+            apt install -y ./slack-desktop-*.deb
+            rm ./slack-desktop-*
+          ;;
+          (*"Steam"*)
+            wget -nc https://steamcdn-a.akamaihd.net/client/installer/steam.deb
+            apt install -y ./steam.deb
+          ;;
+          (*"Telegram"*)
+            snap install telegram-desktop
+          ;;
+          (*"Qbittorrent"*)
+            apt-get install -y qbittorrent
+          ;;
+          (*"VsCode"*)
+            apt-get install -y software-properties-common apt-transport-https
+            wget -qO- -nc https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+            install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+            sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+            apt update 
+            apt-get install -y code
+            rm packages.microsoft.gpg
+          ;;
+          (*"Zoom"*)
+            wget -nc https://zoom.us/client/latest/zoom_amd64.deb
+            apt install -y ./zoom_amd64.deb
+            rm ./zoom_amd64.deb
+          ;;
+          (*)
+            printf "[Warning] Option not reconigzed %s \n" $pkg
+          ;;
+        esac
     printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
   done
 }
 
+####---------------------------------------SCREENS-----------------------------------------------------
+
 ##Screen showing all software and confirm instalation
 function screenInstalation(){
-  whiptail --scrolltext "The following software will be installed ${arrayOptions[@]}" 50 80
-  pkgs=""
+    whiptail --title "$TITLE" --textbox /dev/stdin 25 80 <<<"$(printf "The following software will be installed: \n\n$(printOptions)" )"
+    getOnlyKeys
+    #echo ${pkgs[@]}
 }
 
 ##Screen for select software
@@ -137,7 +163,7 @@ function screenSelectSoft(){
     3>&1 1<&2 2>&3)
 }
 
-#-----------------------------------------------------------------------------------------------#
+#-----------------------------------------MAIN-----------------------------------------------------#
 ##Main structure
 ADVSEL=$(whiptail --title "$TITLE" --menu "Choose an option" 15 60 4 \
     "1" "Install all software" \
